5443525e67de6b33b95eeb845d974280e0e0f038
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-18 13:47:51 +0300

    Delete 5.py

--- 5.py
+++
@@ -1,7 +0,0 @@
-import datetime
-def printTimeStamp(name):
-    print('Автор програми: ' + name)
-    print('Час компіляції: ' + str(datetime.datetime.now()))
-name=str(input("Print your name: "))
-print("Hello,"+name)
-printTimeStamp("Давлат Черновол , Сергій Михайлов ")
