e9ca0aed4b39f5203601a88f28b24f5cc0697c43
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-18 12:15:49 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,10 @@
 import datetime
 def printTimeStamp(name):
- print('Автор програми: ' + name)
+ print('Автори програми: ' + name)
  print('Час компіляції: ' + str(datetime.datetime.now()))
 
 a=input("Number of containers below 1 liter : ")
 b=input("Number of containers biger 1 liter : ")
 print("You get "+"%.2f" % (int(a)*float(0.10)+int(b)*float(0.25))+"$")
 
-printTimeStamp("Давлат Чорновол , Сергей Михайлов")
+printTimeStamp("Давлат Чорновол , Сергій Михайлов")
