34adec3f39540143dff733ee85c6231dbe3c0d4e
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:19:03 +0300

    Update 19.py

--- 1A/19.py
+++ 1A/19.py
@@ -2,8 +2,8 @@ import datetime
 import time
 
 def printTimeStamp(name):
-    print('Автори програми: ' + name)
+    print('Автор програми: ' + name)
     print('Час компіляції: ' + str(datetime.datetime.now()))
 
 print("%s"%time.asctime())
-printTimeStamp("Давлат Чорновол,Сергій Михайлов")
+printTimeStamp("Сергій Михайлов")
