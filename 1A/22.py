bd3a776bb2d94c49b25995d5c1a11249bc1ab2d3
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:20:43 +0300

    Update 22.py

--- 1A/22.py
+++ 1A/22.py
@@ -2,8 +2,8 @@ import datetime
 import math
 
 def printTimeStamp(name):
-    print('Автори програми: ' + name)
+    print('Автор програми: ' + name)
     print('Час компіляції: ' + str(datetime.datetime.now()))
 r = int(input("Введите радиус: "))
 print("Площа круга с указанным радиусом: ", math.pi * math.pow(r, 2), "\nОбъем шара с указанным радиусом: ", 4/3 * (math.pi * math.pow(r, 3)))
-printTimeStamp("Давлат Чорновол,Сергій Михайлов")
+printTimeStamp("Сергій Михайлов")
