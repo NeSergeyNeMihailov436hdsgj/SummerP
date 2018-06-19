94706d54653bb5bd2307fbf90ea52857b0eab3bb
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:18:30 +0300

    Update 17.py

--- 1A/17.py
+++ 1A/17.py
@@ -2,7 +2,7 @@ import datetime
 
 
 def printTimeStamp(name):
-    print('Автори програми: ' + name)
+    print('Автор програми: ' + name)
     print('Час компіляції: ' + str(datetime.datetime.now()))
 
 money=int(input("Money:"))
@@ -10,4 +10,4 @@ first_year=money*(1+float(0.14)*1)
 second_year=money*(1+float(0.14)*2)
 third_year=money*(1+float(0.14)*3)
 print("First year "+"%2.f"%(first_year)+"\n"+"Second year "+"%2.f"%(second_year)+"\n"+"Third year "+"%2.f"%(third_year)+"\n")
-printTimeStamp("Давлат Чорновол,Сергій Михайлов")
+printTimeStamp("Сергій Михайлов")
