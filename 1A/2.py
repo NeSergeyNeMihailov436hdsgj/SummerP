4427caf924e302989df9dd5278ba254cdb1d8967
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:19:24 +0300

    Update 2.py

--- 1A/2.py
+++ 1A/2.py
@@ -1,6 +1,6 @@
 import datetime
 def printTimeStamp(name):
-    print('Автори програми: ' + name)
+    print('Автор програми: ' + name)
     print('Час компиляції: ' + str(datetime.datetime.now()))
 pod = list(map(float,input("Укажите рост в сантиметрах и вес в киллограммах через пробел: ").split()))
 res = pod[1] / (pod[0] * pod[0])
@@ -8,4 +8,4 @@ result = list (str (res))
 del result[0], result[0], result[0], result[0] #В результате подсчета ИМТ всегда убираются ноли
 result.insert(2, ".")
 print ("IMT: ", (''.join(result)))
-printTimeStamp("Давлат Чорновол , Сергій Михайлов ")
+printTimeStamp("Сергій Михайлов ")
