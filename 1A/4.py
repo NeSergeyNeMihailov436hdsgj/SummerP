f585ce4f4885553fe86a73c0391755d1a881de0d
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:23:13 +0300

    Update 4.py

--- 1A/4.py
+++ 1A/4.py
@@ -2,10 +2,10 @@ import datetime
 
 def printTimeStamp(name):
     def printTimeStamp(name):
-        print('Автори програми: ' + name)
+        print('Автор програми: ' + name)
         print('Час компиляції: ' + str(datetime.datetime.now()))
 a=input(" Letter :")
 if  a == "a" or a == "i" or a == "e" or a == "o" or a == "u": print("The letter is vowel.")
 elif a=="y": print("Y - vowel, and sometimes - consonant")
 else: print("The letter is consonant")
-printTimeStamp("Давлат Чорновол ,Сергій Михайлов")
+printTimeStamp("Сергій Михайлов")
