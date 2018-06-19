635d78c541f15e852e9ee43e5fa4d670bda12ad4
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:13:46 +0300

    Update 1.py

--- 1A/1.py
+++ 1A/1.py
@@ -7,4 +7,4 @@ a=input("Number of containers below 1 liter : ")
 b=input("Number of containers biger 1 liter : ")
 print("You get "+"%.2f" % (int(a)*float(0.10)+int(b)*float(0.25))+"$")
 
-printTimeStamp("Давлат Чорновол , Сергій Михайлов")
+printTimeStamp("Сергій Михайлов")
