2f63c21483a7d22dc0d487363039455d5c88d25c
SergeyMihailov <40345080+SergeyMihailov@users.noreply.github.com>
2018-06-19 11:15:50 +0300

    Update 12.py

--- 1A/12.py
+++ 1A/12.py
@@ -4,4 +4,4 @@ def printTimeStamp(name):
     print('Час компіляції: ' + str(datetime.datetime.now()))
 data = list(map(int,input("Введите промежуток времени в формате ДНИ-ЧАСЫ-МИНУТЫ-СЕКУНДЫ через пробел: ").split()))
 print ("Результат в секундах:", (86400 * data[0]) + (3600 * data[1]) + (60 * data[2]) + data[3])
-printTimeStamp("Давлат Чорновол,Сергій Михайлов")
+printTimeStamp("Сергій Михайлов")
