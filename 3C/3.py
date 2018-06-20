6b5b49563247c2ad2e79e8721e533998b1c6dc2d
Sergey Mihailov <40345080+NeSergeyNeMihailov436hdsgj@users.noreply.github.com>
2018-06-20 19:23:53 +0300

    Update and rename 3c.py to 3.py

--- 3C/3.py
+++ 3C/3.py
@@ -1,11 +1,9 @@
 import turtle
 import datetime
-
 def printTimeStamp(name):
     print('Автор програми: ' + name)
     print('Час компіляції: ' + str(datetime.datetime.now()))
-printTimeStamp('Сергей Михайлов')
-
+    printTimeStamp('Сергей Михайлов')
 def drawTriangle(points,color,myTurtle):
     myTurtle.fillcolor(color)
     myTurtle.up()
@@ -16,10 +14,8 @@ def drawTriangle(points,color,myTurtle):
     myTurtle.goto(points[2][0],points[2][1])
     myTurtle.goto(points[0][0],points[0][1])
     myTurtle.end_fill()
-
 def getMid(p1,p2):
     return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
-
 def sierpinski(points,degree,myTurtle):
     colormap = ['violet','yellow','white','green','red',
                 'blue']
@@ -37,12 +33,10 @@ def sierpinski(points,degree,myTurtle):
                         getMid(points[2], points[1]),
                         getMid(points[0], points[2])],
                    degree-1, myTurtle)
-
 def main():
    myTurtle = turtle.Turtle()
    myWin = turtle.Screen()
    myPoints = [[-200,-150],[0,200],[200,-150]]
    sierpinski(myPoints,5,myTurtle)
    myWin.exitonclick()
-
 main()
