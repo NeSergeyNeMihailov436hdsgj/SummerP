f85274c3b55c60102f144ee99ceaf2c02a2b94fb
Sergey Mihailov <40345080+NeSergeyNeMihailov436hdsgj@users.noreply.github.com>
2018-06-20 20:41:17 +0300

    Add files via upload

---
+++ 1A/13.py
@@ -0,0 +1,12 @@
+﻿import datetime
+
+
+def printTimeStamp(name):
+    print('Автор програми: ' + name)
+    print('Час компіляції: ' + str(datetime.datetime.now()))
+
+inp=(input("Numbers:").split(" "))
+a=(inp)
+midle=(int(a[0])+int(a[1])+int(a[2])-int(min(a))-int(max(a)))
+print(min(a),str(midle),max(a))
+print("Сергій Михайлов")
