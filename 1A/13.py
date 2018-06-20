c0957f76d72dd0515172ff2bfa67cb06361107e0
Sergey Mihailov <40345080+NeSergeyNeMihailov436hdsgj@users.noreply.github.com>
2018-06-20 20:43:18 +0300

    Add files via upload

---
+++ 1A/13.py
@@ -0,0 +1,8 @@
+﻿import datetime
+def printTimeStamp(name):
+    print('Автор программы: ' + name)
+    print('Время компиляции: ' + str(datetime.datetime.now()))
+numbers = list (map(int,input("Введите 3 целых числа через пробел: ").split()))
+numbers = sorted(numbers)
+print("Значение между максимумом и минимумом: ",(numbers[0] + numbers[1] + numbers[2]) - min(numbers) - max(numbers))
+printTimeStamp("Сергій Михайлов")
