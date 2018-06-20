31960769394ed6b1dfd5ed9ae587d4a9143f2b0e
Sergey Mihailov <40345080+NeSergeyNeMihailov436hdsgj@users.noreply.github.com>
2018-06-20 19:04:43 +0300

    Update and rename 18a.py to 18.py

--- 2A/18.py
+++ 2A/18.py
@@ -2,8 +2,8 @@ import datetime
 def printTimeStamp(name):
     print('Автор програми: ' + name)
     print('Час компіляції: ' + str(datetime.datetime.now()))
-printTimeStamp('Давлат Черновол')
 tuple1 = [('A', 1), ('B', 2), ('C', 3)]
 print(tuple1)
 dict1 = dict([('A', 1), ('B', 2), ('C', 3)])
 print(dict1)
+printTimeStamp('Михайлов Сергій')
