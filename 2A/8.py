73a7c287cbce5f6c74f2a413ea867d706d34655e
Sergey Mihailov <40345080+NeSergeyNeMihailov436hdsgj@users.noreply.github.com>
2018-06-20 18:54:04 +0300

    Update and rename 8 задание.py to 8.py

--- 2A/8.py
+++ 2A/8.py
@@ -3,8 +3,8 @@ import datetime
 def printTimeStamp(name):
     print("Автор програми: " + name)
     print("Час компіляції: " + str(datetime.datetime.now()),"\n")
-printTimeStamp("Давлат Черновол")
 
 x=list(map(str,input()))
 myset=set(x)
 print(myset)
+printTimeStamp("Михайлов Сергій")
