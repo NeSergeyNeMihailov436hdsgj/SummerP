9a528e50ca3a41393b223474c7afb2893c68254c
Sergey Mihailov <40345080+NeSergeyNeMihailov436hdsgj@users.noreply.github.com>
2018-06-20 20:14:59 +0300

    Update 2.py

--- 1A/2.py
+++ 1A/2.py
@@ -5,7 +5,7 @@ def printTimeStamp(name):
 pod = list(map(float,input("Укажите рост в сантиметрах и вес в киллограммах через пробел: ").split()))
 res = pod[1] / (pod[0] * pod[0])
 result = list (str (res))
-del result[0], result[0], result[0], result[0] #В результате подсчета ИМТ всегда убираются ноли
+del result[0], result[0], result[0], result[0] #В результате подсчета ИМТ всегда убираются нули
 result.insert(2, ".")
 print ("IMT: ", (''.join(result)))
 printTimeStamp(" Сергій Михайлов ")
